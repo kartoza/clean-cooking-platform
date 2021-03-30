deb5fb843285c2a76d727d21e492085b84ccb778
dimasciput <dimas.ciputra@gmail.com>
2021-03-30 16:48:40 +0700

    Update templates

--- django_project/custom/urls.py
+++ django_project/custom/urls.py
@@ -3,6 +3,7 @@
 
 from django.conf.urls import url
 from django.urls import include
+from django.views.generic import TemplateView
 from custom.views import HomeView, map_view_with_slug, ToolView
 
 urlpatterns = [
@@ -13,6 +14,15 @@ urlpatterns = [
         include('custom.api_urls')),
     url(r'^tool/$',
         view=ToolView.as_view(),
+        name='tool'),
+    url(r'^methodology/$',
+        view=TemplateView.as_view(template_name='eae/methodology.html'),
+        name='tool'),
+    url(r'^about-us/$',
+        view=TemplateView.as_view(template_name='eae/about.html'),
+        name='tool'),
+    url(r'^user-stories/$',
+        view=TemplateView.as_view(template_name='eae/user-stories.html'),
         name='tool'),
     url(r'^view/(?P<slug>[^/]+)$',
         view=map_view_with_slug,
