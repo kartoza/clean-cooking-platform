3898064ba8159bfba0fcc9fc537a870ae30bc751
Dimas <dimas.ciputra@gmail.com>
2021-04-13 13:57:22 +0700

    Update theme

--- django_project/custom/static/eae/tool/controls.js
+++ django_project/custom/static/eae/tool/controls.js
@@ -628,8 +628,13 @@ cursor: pointer;
 	set_default(sl.input);
 };
 
-function sort_datasets(config) {
-	const {sort_datasets, sort_subbranches, sort_branches} = config;
+function 	sort_datasets(config) {
+	try {
+		const {sort_datasets, sort_subbranches, sort_branches} = config;
+	} catch (e) {
+		console.log(e);
+		return false;
+	}
 
 	const controls_elements = qsa('ds-controls', contents_el);
 
