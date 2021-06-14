7effb7ea6bee5ec4b5dd8636e39a81987d1b4daa
dimasciput <dimas.ciputra@gmail.com>
2021-06-14 14:49:48 +0700

    Improve Geography Admin Panel

--- django_project/custom/models/geography.py
+++ django_project/custom/models/geography.py
@@ -1,9 +1,14 @@
 # coding=utf-8
 """Geography model definition.
 """
+import requests
+import re
 from django.contrib.gis.db import models
+from django.db.models.signals import post_save
 from django.conf import settings
+from django.dispatch import receiver
 from django.contrib.postgres.fields import JSONField
+from geonode.layers.models import Layer
 
 
 class Geography(models.Model):
@@ -29,8 +34,8 @@ class Geography(models.Model):
     cca3 = models.CharField(
         max_length=3,
         blank=False,
-        verbose_name='CCA3',
-        help_text='Three-letter country codes'
+        verbose_name='ISO 3 Letter Country Code',
+        help_text='Three-letter country code'
     )
 
     circle = models.CharField(
@@ -61,6 +66,22 @@ class Geography(models.Model):
         upload_to="boundary/"
     )
 
+    vector_boundary_layer = models.ForeignKey(
+        Layer,
+        null=True,
+        blank=True,
+        related_name='vector_boundary_layer',
+        on_delete=models.SET_NULL
+    )
+
+    raster_mask_layer = models.ForeignKey(
+        Layer,
+        null=True,
+        blank=True,
+        related_name='raster_mask_layer',
+        on_delete=models.SET_NULL
+    )
+
     configuration = JSONField(
         null=True,
         blank=True
@@ -70,6 +91,7 @@ class Geography(models.Model):
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         null=True,
+        blank=True,
         on_delete=models.SET_NULL,
         related_name='geography_created_by'
     )
@@ -78,6 +100,7 @@ class Geography(models.Model):
     updated_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         null=True,
+        blank=True,
         on_delete=models.SET_NULL,
         related_name='geography_updated_by'
     )
@@ -96,3 +119,32 @@ class Geography(models.Model):
 
     def __str__(self):
         return self.name
+
+
+@receiver(post_save, sender=Geography)
+def post_save_geography(sender, instance, **kwargs):
+    if not instance.raster_mask_layer:
+        return
+
+    if (
+            instance.raster_mask_layer
+    ):
+        post_save.disconnect(post_save_geography, sender=sender)
+        geoserver = settings.GEOSERVER_LOCATION
+        url = (
+            f'{geoserver}wcs?service=WCS&version=2.0.1&request='
+            f'describecoverage&'
+            f'coverageid={instance.raster_mask_layer.typename}'
+        )
+        r = requests.get(url = url)
+
+        data = r.text
+        grid_envelope = (
+            re.findall('<gml:high>(.*?)</gml:high>', data, re.DOTALL)
+        )
+        if len(grid_envelope) > 0:
+            x, y = grid_envelope[0].split(' ')
+            instance.boundary_dimension_x = int(x)
+            instance.boundary_dimension_y = int(y)
+            instance.save()
+        post_save.connect(post_save_geography, sender=sender)
