b6f7efe0baa72f2c5276b7ed23972266adb4d02e
Dimas Ciputra <dimas.ciputra@gmail.com>
2021-06-23 11:14:54 +0700

    Change projection to 4326 (#79)

--- django_project/custom/serializers/dataset_serializer.py
+++ django_project/custom/serializers/dataset_serializer.py
@@ -23,10 +23,12 @@ def geonode_layer_links(geonode_layer, geography):
     )
     layer_url = None
     if links.exists():
+        url = links[0].url
+        url = url.replace('3857', '4326')
         x = geography.boundary_dimension_x
         y = geography.boundary_dimension_y
         layer_url = '/proxy_cca/{url}&SCALESIZE=i({x}),j({y})'.format(
-            url=links[0].url,
+            url=url,
             x=x,
             y=y
         )
