15a3795659f1d74a09abdd238cd81eef449b11ec
Dimas Ciputra <dimas.ciputra@gmail.com>
2021-11-01 15:16:45 +0700

    Add bread crumb (#149)

--- django_project/custom/views/preset.py
+++ django_project/custom/views/preset.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 
-from custom.models import UseCase
+from custom.models import UseCase, Geography
 
 
 class UseCaseView(TemplateView):
@@ -9,4 +9,9 @@ class UseCaseView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(UseCaseView, self).get_context_data(**kwargs)
         context['use_cases'] = UseCase.objects.all()
+        context['geo'] = Geography.objects.get(
+            id=self.request.GET.get('geoId')
+        )
+        if self.request.GET.get('subRegion', None):
+            context['subRegion'] = self.request.GET.get('subRegion').split(':')
         return context
