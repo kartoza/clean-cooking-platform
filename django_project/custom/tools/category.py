b1ebb694f8dc42cf64d65a340d6f89f2c7b8523b
dimasciput <dimas.ciputra@gmail.com>
2022-03-15 16:30:53 +0700

    Show importance and domain in pdf report

--- django_project/custom/tools/category.py
+++ django_project/custom/tools/category.py
@@ -4,14 +4,24 @@ from custom.models.category import Category
 
 def category_from_url(inputs):
     """Returns list of categories from parameter inputs"""
-    name_filter = '('
+    categories = []
     for input_name in inputs:
         input_name = input_name.lower()
+        domain = re.search(r'\((.*?)\)', input_name)
+        if domain:
+            domain = domain.group(1)
+        weight = re.search(r'\[(.*?)\]', input_name)
+        if weight:
+            weight = weight.group(1)
         if input_name != 'population-density':
             input_name = input_name.replace('-', ' ')
         input_name = re.sub("[\(\[].*?[\)\]]", "", input_name)
-        name_filter += input_name + '|'
-    name_filter = name_filter[:-1] + ')'
-    return Category.objects.filter(
-        name_long__iregex=r'{}'.format(name_filter)
-    )
+        if Category.objects.filter(name_long__icontains=input_name):
+            categories.append({
+                'domain': domain,
+                'weight': weight,
+                'category': Category.objects.filter(
+                    name_long__icontains=input_name
+                ).first()
+            })
+    return categories
